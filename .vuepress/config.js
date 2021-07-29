96c77671bc53ec75a9a318246c1a15d971c2cb73
jzlxx <1328741496@qq.com>
2021-07-29 11:51:01 +0800

    chore:目录调整

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,48 +18,48 @@ module.exports = {
     searchMaxSuggestions: 10,
     sidebar: {
       // 链优品
-      "/api/lianyoupin/": getLypinAppSider("移动端", "规范"),
-      "/admin/lianyoupin/": getLypAdminSider("后台管理", "说明"),
-      "/merchant/": getLypMerchantSider("商户管理", "说明"),
+      // "/api/lianyoupin/": getLypinAppSider("移动端", "规范"),
+      // "/admin/lianyoupin/": getLypAdminSider("后台管理", "说明"),
+      // "/merchant/": getLypMerchantSider("商户管理", "说明"),
       // Imeet
-      "/api/imeet/": getImeetAppSider("移动端", "规范"),
-      "/admin/imeet/": getImeetAdminSider("后台管理", "说明"),
+      // "/api/imeet/": getImeetAppSider("移动端", "规范"),
+      // "/admin/imeet/": getImeetAdminSider("后台管理", "说明"),
       // AChat
-      "/api/achat/": getAchatAppSider("移动端", "规范"),
-      "/admin/achat/": getAchatAdminSider("后台管理", "说明"),
+      // "/api/achat/": getAchatAppSider("移动端", "规范"),
+      // "/admin/achat/": getAchatAdminSider("后台管理", "说明"),
       // Schain
-      "/api/schain/": getSchainAppSider("移动端", "规范"),
-      "/admin/schain/": getSchainAdminSider("后台管理", "说明"),
+      // "/api/schain/": getSchainAppSider("移动端", "规范"),
+      // "/admin/schain/": getSchainAdminSider("后台管理", "说明"),
       // 科创链
-      "/api/kcstore/": getKcStoreAppSider("移动端", "规范"),
-      "/admin/kcstore/": getKcStoreAdminSider("后台管理", "规范"),
-      "/merchant/kcstore/": getKcStoreMerchantSider("商户管理", "规范"),
+      // "/api/kcstore/": getKcStoreAppSider("移动端", "规范"),
+      // "/admin/kcstore/": getKcStoreAdminSider("后台管理", "规范"),
+      // "/merchant/kcstore/": getKcStoreMerchantSider("商户管理", "规范"),
       // 火推
-      "/api/ht/": getHtAppSider("移动端", "规范"),
-      "/admin/ht/": getHtAdminSider("后台管理", "说明"),
+      // "/api/ht/": getHtAppSider("移动端", "规范"),
+      // "/admin/ht/": getHtAdminSider("后台管理", "说明"),
       // 创业吧
-      "/api/cyb/": getCybAppSider("移动端", "规范"),
-      "/admin/cyb/": getCybAdminSider("后台管理", "说明"),
+      // "/api/cyb/": getCybAppSider("移动端", "规范"),
+      // "/admin/cyb/": getCybAdminSider("后台管理", "说明"),
       // 创业吧
-      "/api/mt/": getMtAppSider("移动端", "规范"),
-      "/admin/mt/": getMtAdminSider("后台管理", "规范"),
+      // "/api/mt/": getMtAppSider("移动端", "规范"),
+      // "/admin/mt/": getMtAdminSider("后台管理", "规范"),
       // 合众
-      "/api/hz/": getHzAppSider("移动端", "规范"),
-      "/admin/hz/": getHzAdminSider("后台管理", "说明"),
+      // "/api/hz/": getHzAppSider("移动端", "规范"),
+      // "/admin/hz/": getHzAdminSider("后台管理", "说明"),
       // 海马
-      "/api/hashorse/": getHashorseAppSider("移动端", "规范"),
-      "/admin/hashorse/": getHashorseAdminSider("后台管理", "说明"),
+      // "/api/hashorse/": getHashorseAppSider("移动端", "规范"),
+      // "/admin/hashorse/": getHashorseAdminSider("后台管理", "说明"),
       // 人人算力
-      "/api/hashevery/": getHasheveryAppSider("移动端", "规范"),
-      "/admin/hashevery/": getHasheveryAdminSider("后台管理", "说明"),
+      // "/api/hashevery/": getHasheveryAppSider("移动端", "规范"),
+      // "/admin/hashevery/": getHasheveryAdminSider("后台管理", "说明"),
       // 标准模板
       "/api/template/": getTemplateAppSider("移动端", "规范"),
       "/admin/template/": getTemplateAdminSider("后台管理", "说明"),
       // 甬力区
-      "/api/ylq/": getYlqAppSider("移动端", "规范"),
-      "/admin/ylq/": getYlqAdminSider("后台管理", "说明"),
+      // "/api/ylq/": getYlqAppSider("移动端", "规范"),
+      // "/admin/ylq/": getYlqAdminSider("后台管理", "说明"),
       // 蚁创科技
-      "/api/yckj/": getYckjAppSider("移动端", "规范"),
+      // "/api/yckj/": getYckjAppSider("移动端", "规范"),
       //saas管理后台
       "/admin/saas/": getSaasSAdminSider("管理后台", "规范"),
     }
